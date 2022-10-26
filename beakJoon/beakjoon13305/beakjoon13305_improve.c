d2e529b2f779d33c1c47a2e106a07f29c20b05dc
allThumbsGenius <butijung@naver.com>
2022-10-26 17:09:26 +0900

    1.1v

---
+++ beakJoon/beakjoon13305/beakjoon13305_improve.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n;
+	int *distanceBetweenCity;
+	int oilPricePerCity;
+
+	scanf("%d", &n);
+
+	distanceBetweenCity = (int*)malloc(n - 1 * sizeof(int));
+
+	for(int i = 0; i < n - 1; ++i){
+		scanf("%d", &distanceBetweenCity[i]);
+	}
+
+	scanf("%d", &oilPricePerCity);
+	int minPrice = oilPricePerCity;
+	int totalPrice = 0;
+	for(int i = 0; i < n -1; ++i, scanf("%d", &oilPricePerCity)){
+		if(minPrice > oilPricePerCity){
+			minPrice = oilPricePerCity;
+		}
+		totalPrice += minPrice * distanceBetweenCity[i];
+	}
+
+	printf("%d", totalPrice);
+
+	free(distanceBetweenCity);
+
+	return 0;
+}
