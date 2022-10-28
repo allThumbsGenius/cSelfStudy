b09aa5381c5df41e20d57e258440524311157182
allThumbsGenius <butijung@naver.com>
2022-10-28 22:18:23 +0900

    1.1v made by other way that simmilar with divide and conquer

--- beakJoon/greedyAlgorithm/beakjoon1715/beakjoon1715.c
+++ beakJoon/greedyAlgorithm/beakjoon1715/beakjoon1715.c
@@ -1,50 +1,61 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int compare(const void *a, const void *b){
-	int *pa = (int*)a;
-	int *pb = (int*)b;
-
-	if(*pa > *pb){
-		return 1;
-	}
-	else if(*pa < *pb){
-		return -1;
-	}
-	else{
-		return 0;
-	}
-}
 
 int main(){
-	int cardCount[100000];
+	int card[100000];
 	int N;
-
-	scanf("%d", &N);
+	int sum = 0;
+	const int IS_USED = 1001;
 	
-	for(int i = 0; i < N; i++){
-		scanf("%d", &cardCount[i]);
-	}
+	scanf("%d", &N);
 
-	qsort(cardCount, N, sizeof(int), compare);
+	for(int i = 0; i < N; i++){
+		scanf("%d", &card[i]);
+	}
 
 	if(N == 1){
-		printf("%d", cardCount[0]);
+		printf("%d", card[0]);
 		return 0;
 	}
 
-	if(N == 2){
-		printf("%d", cardCount[0] + cardCount[1]);
-		return 0;
-	}
-	
-	int sum = 0;
-	sum += (N - 1) * (cardCount[0] + cardCount[1]);
-	for(int i = 2; i < N; i++){
-		sum += (N - i) * cardCount[i];
-	}
+	for(int i = 0; i < N - 1; i++){
+		int min[2];
+		int tempIndex[2] = {-1, -1};
 
+		min[0] = card[0];
+		
+		int j = 0;
+		for(j = 1; j < N; j++){
+			if(min[0] > card[j]){
+				min[0] = card[j];
+				tempIndex[0] = j;
+				card[j] = IS_USED;
+			}
+		}
+		if(tempIndex[0] == -1){
+			card[0] = IS_USED;
+			tempIndex[0] = 0;
+		}
+
+		min[1] = card[0];
+		for(j = 0; j < N; j++){
+			if(min[1] > card[j]){
+				min[1] = card[j];
+				tempIndex[1] = j;
+				card[j] = IS_USED;
+			}
+		}
+
+		sum += (min[0] + min[1]);
+
+		if(tempIndex[0] > tempIndex[1]){
+			card[tempIndex[1]] = sum;
+		}
+		else{
+			card[tempIndex[0]] = sum;
+		}
+	}
 	printf("%d", sum);
-	
+
 	return 0;
 }
+
