d5b75763d10f7009702092fd3bef1fc200539af3
allThumbsGenius <butijung@naver.com>
2022-10-28 16:10:56 +0900

    1.1v changed logic of input and calculating N % 3 == 0

---
+++ beakJoon/greedyAlgorithm/beakjoon10610/beakjoon10610_improved.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	int digitNumberCount[10] = {0};
+	char ch;
+
+	while((ch = getchar()) != '\n'){
+		digitNumberCount[ch - '0']++;
+	}
+
+	if(digitNumberCount[0] == 0 || (((digitNumberCount[2] + digitNumberCount[5] + digitNumberCount[8]) * 2) + digitNumberCount[1] + digitNumberCount[4] + digitNumberCount[7]) % 3 != 0){
+		printf("-1");
+		return 0;
+	}
+	
+	for(int i = 9; i >= 0; --i){
+		for(int j = digitNumberCount[i]; j > 0; --j){
+			printf("%d", i);
+		}
+	}
+
+	return 0;
+}
