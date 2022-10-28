1b7f83daf5f593551170f7d1bc32b8835ccf2f8a
allThumbsGenius <butijung@naver.com>
2022-10-28 20:51:35 +0900

    1.5v finally done it boom!

---
+++ beakJoon/greedyAlgorithm/beakjoon1946/beakjoon1946_GoodAnswer.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int testcaseCount;
+	int candidateCount;
+
+	int ranks[100001];
+	
+	scanf("%d", &testcaseCount);
+
+	for(int i = 0; i < testcaseCount; i++){
+		scanf("%d", &candidateCount);
+		
+		for(int j = 0; j < candidateCount; j++){
+			int k;
+			scanf("%d", &k);
+			scanf("%d", &ranks[k]);
+		}
+		
+		int count = 1;
+		int min = ranks[1];
+		for(int j = 2; j < candidateCount; j++){
+			if(min > ranks[j]){
+				count++;
+				min = ranks[j];
+			}
+		}
+
+		printf("%d\n", count);
+	}
+
+
+	return 0;
+}
