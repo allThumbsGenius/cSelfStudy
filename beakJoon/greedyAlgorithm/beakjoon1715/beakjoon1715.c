dcca9689279295621a29e5b75a1ae146836cfbf0
allThumbsGenius <butijung@naver.com>
2022-11-01 23:49:45 +0900

    1.3v use minHeap to make in time to solve the problem (using good datastructure is important)

--- beakJoon/greedyAlgorithm/beakjoon1715/beakjoon1715.c
+++ beakJoon/greedyAlgorithm/beakjoon1715/beakjoon1715.c
@@ -1,62 +1,88 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+	int array[100001];
+	int size;
+}Heap;
+
+void initHeap(Heap* heap, int size){
+	heap->size = size;
+}
+
+void minHeapify(Heap *heap, int index){
+	int smallest = index;
+	int leftChild = index * 2;
+	int rightChild = index * 2 + 1;
+	
+	if(leftChild <= heap->size && heap->array[smallest] > heap->array[leftChild]){
+		smallest = leftChild;
+	}
+	if(rightChild <= heap->size && heap->array[smallest] > heap->array[rightChild]){
+		smallest = rightChild;
+	}
+	if(smallest != index){
+		int temp = heap->array[index];
+		heap->array[index] = heap->array[smallest];
+		heap->array[smallest] = temp;
+		minHeapify(heap, smallest);
+	}
+}
+
+int minExtract(Heap *heap){
+	int ret = heap->array[1];
+	heap->array[1] = heap->array[heap->size--];
+	minHeapify(heap, 1);
+
+	return ret;
+}
+
+void heapInsert(Heap *heap, int value){
+	int index = ++heap->size;
+	int parent = index / 2;
+
+	heap->array[index] = value;
+	while(heap->array[index] < heap->array[parent]){
+		int temp = heap->array[index];
+		heap->array[index] = heap->array[parent];
+		heap->array[parent] = temp;
+	}
+}
 
 int main(){
-	int card[100000];
 	int N;
 	int sum = 0;
-	const int IS_USED = 1001;
+	Heap *heap;
 	
+	heap = (Heap *)malloc(sizeof(Heap) * 1);
+
 	scanf("%d", &N);
 
-	for(int i = 0; i < N; i++){
-		scanf("%d", &card[i]);
+	initHeap(heap, N);
+	for(int i = 1; i <= N; i++){
+		scanf("%d", &heap->array[i]);
 	}
-
+	
 	if(N == 1){
-		printf("%d", card[0]);
+		printf("%d", heap->array[1]);
 		return 0;
 	}
 
+	//build heap
+	for(int i = N / 2; i >= 1; i--){
+		minHeapify(heap, i);
+	}
+
+
 	for(int i = 0; i < N - 1; i++){
 		int min[2];
-		int tempIndex[2] = {-1, -1};
-
+		int j;
 		
-		int j = 0;
-		min[0] = card[0];
-		for(j = 1; j < N; j++){
-			if(min[0] > card[j]){
-				min[0] = card[j];
-				tempIndex[0] = j;
-			}
-		}
-		if(tempIndex[0] == -1){
-			card[0] = IS_USED;
-			tempIndex[0] = 0;
-		}
-		card[tempIndex[0]] = IS_USED;
-
-		min[1] = card[0];
-		for(j = 1; j < N; j++){
-			if(min[1] > card[j]){
-				min[1] = card[j];
-				tempIndex[1] = j;
-			}
-		}
-		if(tempIndex[1] == -1){
-			card[0] = IS_USED;
-			tempIndex[1] = 0;
-		}
-		card[tempIndex[1]] = IS_USED;
-
+		min[0] = minExtract(heap);
+		min[1] = minExtract(heap);
+		heapInsert(heap, min[0] + min[1]);
 		sum += (min[0] + min[1]);
 
-		if(tempIndex[0] > tempIndex[1]){
-			card[tempIndex[1]] = min[0] + min[1];
-		}
-		else{
-			card[tempIndex[0]] = min[0] + min[1];
-		}
 
 	}
 	printf("%d", sum);
