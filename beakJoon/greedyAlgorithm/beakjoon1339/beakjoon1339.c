6f9cebce93f149a970d3585be29462ce96bc8c6c
allThumbsGenius <butijung@naver.com>
2022-11-05 20:18:07 +0900

    1.0v first note and get how to solve problem and do coding man

---
+++ beakJoon/greedyAlgorithm/beakjoon1339/beakjoon1339.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int compare(const void *a, const void *b){
+	unsigned int *pa = (unsigned int *)a;
+	unsigned int *pb = (unsigned int *)b;
+
+	if(*pa > *pb){
+		return -1;
+	}
+	else if(*pa < *pb){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+unsigned int getPow(int p, int k){
+	unsigned int temp = 1;
+	for(int i = 0; i < k; i++){
+		temp *= p;
+	}
+	return temp;
+}
+
+int main(){
+	int n;
+	unsigned int alphabet[26] = {0};
+	char inputs[8];
+	unsigned int answer = 0;
+
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		scanf("%s", inputs);
+		int len = strlen(inputs);
+		for(int j = 0; j < len; j++){
+			alphabet[inputs[j] - 'A'] += getPow(10, len - 1 - j);
+		}
+	}
+
+
+	qsort(alphabet, 26, sizeof(unsigned int), compare);
+
+	for(int i = 9, j = 0; i > 0 ; i--, j++){
+		answer += (alphabet[j] * i);
+	}
+
+	printf("%d", answer);
+
+	return 0;
+}
